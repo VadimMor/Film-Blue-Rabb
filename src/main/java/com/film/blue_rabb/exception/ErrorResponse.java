a76dd199c8335c4cf6d26ab9911e772ca5f0cb3c
Blue-Rabb <rich246135@gmail.com>
2024-08-12 01:35:23 +0300

    basic exception +

---
+++ src/main/java/com/film/blue_rabb/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.film.blue_rabb.exception;
+
+import java.time.LocalDateTime;
+
+
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        int status,
+        String error,
+        String message
+) {
+}
