d975d00fac3dd5e82a4be02eef4879eba66df8f8
Vadim Uniatiskiy <87434747+VadimMor@users.noreply.github.com>
2024-12-05 03:47:25 +0300

    Добавлен dockerfile

--- frontend/src/components/Header/NavLink/NavLinks.js
+++ frontend/src/components/Header/NavLink/NavLinks.js
@@ -1,4 +1,4 @@
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavLink.module.css'
 import '@assets/fonts/iconmoon/style.css'
